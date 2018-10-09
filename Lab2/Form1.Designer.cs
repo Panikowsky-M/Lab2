c0d331aac58953c3849bee8057af634dfe9389ec
Михаил Паниковский <panikowsky.m@gmail.com>
2018-10-09 23:56:27 +0300

    Mirz-Lab2-sln

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(299, 112);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(99, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Движение вкл";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(299, 89);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Движение выкл";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вызов формы 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(59, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 394);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Группировочная панель";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(224, 320);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Удалить перв. стр.";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Добавить строку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Число строк: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(250, 256);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Подсчет строк";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 458);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+	
+        enum Direction { Right, Down, Left, Up };
+        Direction direction = Direction.Right;
+		bool sw,sw2 = false;
+		bool isOpen = true;
+		int count = 0;
+		string msg0 = "Нелязя позвать больше одной формы";
+        string msg1 = "Пустое поле ввода";
+        
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			count++;
+			Form2 newForm = new Form2();
+
+			if (count > 1)
+			{
+				isOpen = false;
+				MessageBox.Show(msg0);
+			}
+			if (isOpen)
+			{
+				
+				newForm.Show();
+				newForm.form1 = this;
+			}
+		}
+
+		public Form1()
+        {
+			
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    label1.Left += 10;
+                    break;
+                case Direction.Down:
+                    label1.Top += 10;
+                    break;
+                case Direction.Left:
+                    label1.Left -= 10;
+                    break;
+                case Direction.Up:
+                    label1.Top -= 10;
+                    break;
+            }
+               if (label1.Left > ClientRectangle.Width - label1.Width)
+			{
+				direction = Direction.Down;
+
+				label1.Left = ClientRectangle.Width - label1.Width;
+			}
+          
+
+			   if(label1.Top > ClientRectangle.Height - label1.Height)
+			    {
+				direction = Direction.Right;
+				label1.Left = ClientRectangle.Width - label1.Width;
+				
+				}
+
+			if (label1.Top > ClientRectangle.Height - label1.Height)
+			{
+				direction = Direction.Left;
+			
+				label1.Top = ClientRectangle.Height - label1.Height;
+			}
+
+			if (label1.Top > 0 && label1.Left < 0)
+			{ 
+				direction = Direction.Up;
+				label1.Top = ClientRectangle.Height- label1.Height;
+				
+			}
+
+			//label1.ForeColor = Color.FromArgb(label1.Left * 255 / (ClientRectangle.Width - label1.Width), 0, 0);
+			  
+			
+			
+		}
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		             {
+			                   sw = false; timer1.Start(); 
+		             }
+
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		           {
+			                   sw = true; timer1.Stop();
+		           }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int count2 = 0;
+            
+            string text0 = textBox1.Text;
+            string[] spln = text0.Split('\n');
+            foreach (var spl in spln)
+            { 
+                count2++;
+            }
+                count2--;
+            label2.Text = "Число строк: " + count2.ToString();
+            if (count2 == 0)
+            {
+                MessageBox.Show(msg1);
+                return;
+            }
+            
+		}
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str0, str1, str2;
+            str1 = "Проверка 2.\r\n";
+            str2 = "Проверка 3.\r\n";
+            str0 = "Проверка.\r\n";
+            string[] textJ = new string[] { str0,str1,str2 };
+            String strJ = String.Join("",textJ);
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sw2 = true;
+        }
+    }
+}
